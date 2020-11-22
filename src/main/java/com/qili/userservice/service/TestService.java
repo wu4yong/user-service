ecf77c843cb9d8ebd3afc00d01681af431f87084
Michael_JAVA <53858500+wu4yong@users.noreply.github.com>
2020-11-22 22:48:19 +0800

    Update TestService.java

--- src/main/java/com/qili/userservice/service/TestService.java
+++ src/main/java/com/qili/userservice/service/TestService.java
@@ -8,5 +8,6 @@ package com.qili.userservice.service;
 public class TestService {
     public static void main(String[] args) {
         System.out.println("我是新增的代码！！！！");
+         System.out.println("我是master修改的代码！！！！");
     }
 }
