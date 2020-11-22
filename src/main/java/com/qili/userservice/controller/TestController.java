55a85b415ab150270081dbd26c7b26a68a2d0536
wu4yong <wu4yong@163.com>
2020-11-23 00:25:02 +0800

    [A]提交develop

---
+++ src/main/java/com/qili/userservice/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.qili.userservice.controller;
+
+/**
+ * @Date: 2020/11/23
+ * @Author: wuyong
+ * @Description: user-service
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("develop controller");
+    }
+}
