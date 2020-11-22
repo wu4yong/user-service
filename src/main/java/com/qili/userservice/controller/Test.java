6481121af9166feeee00262533a9593c93b66ac3
wu4yong <wu4yong@163.com>
2020-11-23 00:34:32 +0800

    [A]提交develop

---
+++ src/main/java/com/qili/userservice/controller/Test.java
@@ -0,0 +1,12 @@
+package com.qili.userservice.controller;
+
+/**
+ * @Date: 2020/11/23
+ * @Author: wuyong
+ * @Description: user-service
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("develop branch!!!");
+    }
+}
