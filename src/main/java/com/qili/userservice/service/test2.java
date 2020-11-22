e3300bbefbb4f00bc7fb6bfc862bbc082fe78d8b
wu4yong <wu4yong@163.com>
2020-11-23 00:15:36 +0800

    [A]提交master！

---
+++ src/main/java/com/qili/userservice/service/test2.java
@@ -0,0 +1,13 @@
+package com.qili.userservice.service;
+
+/**
+ * @Date: 2020/11/23
+ * @Author: wuyong
+ * @Description: user-service
+ */
+public class test2 {
+
+    public static void main(String[] args) {
+        System.out.println("master！！！！！！");
+    }
+}
